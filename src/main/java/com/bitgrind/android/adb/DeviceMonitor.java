49a82aa158b05dd52d52533b3f35d9f97eeff5c9
Mark Renouf <mark.renouf@gmail.com>
2017-03-10 09:18:36 -0500

    Minimally functional device tracker for testing

--- src/main/java/com/bitgrind/android/adb/DeviceMonitor.java
+++
@@ -1,7 +0,0 @@
-package com.bitgrind.android.adb;
-
-/**
- * Created by mrenouf on 03/03/17.
- */
-public class DeviceMonitor {
-}
--- src/main/java/com/bitgrind/android/adb/DeviceTracker.java
+++ src/main/java/com/bitgrind/android/adb/DeviceTracker.java
@@ -1,6 +1,7 @@
 package com.bitgrind.android.adb;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ByteChannel;
 import java.util.concurrent.Executor;
@@ -14,6 +15,7 @@ public class DeviceTracker {
     private final Supplier<Result<ByteChannel>> channelSupplier;
     private final Executor executor;
     boolean shouldExit = false;
+    private boolean running;
 
     public DeviceTracker(Supplier<Result<ByteChannel>> channelSupplier) {
         this(channelSupplier, Executors.newSingleThreadExecutor());
@@ -24,41 +26,45 @@ public class DeviceTracker {
         this.executor = executor;
     }
 
-    public void start() {
-        executor.execute(() -> {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        DeviceTracker tracker = new DeviceTracker(
+                new NetworkChannelSupplier(new InetSocketAddress("127.0.0.1", 5037)));
+        tracker.start();
+        Thread.sleep(10000);
 
+    }
+
+    public synchronized void start() {
+        if (running) {
+            return;
+        }
+        running = true;
+        executor.execute(() -> {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            for (; ; ) {
+                try (Result<ByteChannel> channelResult = channelSupplier.get()) {
+                    if (channelResult.ok()) {
+                        ByteChannel channel = channelResult.get();
+                        writeMessage(channel, buffer, "host:track-devices");
+                        Status status = readStatus(channel, buffer);
+                        if (status.isOk()) {
+                            monitor(channel, buffer);
+                        }
+                    }
+                } catch (IOException e) {
+                }
+                // Delay, try reconnect, try to start ADB, etc
+            }
         });
         shouldExit = true;
     }
 
-    void foo() {
+    private void monitor(ByteChannel channel, ByteBuffer buffer) throws IOException {
+        String initialState = readMessage(channel, buffer);
+        System.err.println("Initial state: " + initialState);
         for (; ; ) {
+            String update = readMessage(channel, buffer);
+            System.err.println("Update: " + update);
         }
-    }
-
-    boolean bar() {
-        try (Result<ByteChannel> channelResult = channelSupplier.get()) {
-            if (channelResult.ok()) {
-                ByteBuffer buffer = ByteBuffer.allocate(1024);
-                ByteChannel channel = channelResult.get();
-
-                try {
-                    writeMessage(channel, buffer, "host:track-devices");
-                    Status status = readStatus(channel, buffer);
-                    if (!status.isOk()) {
-                        return false;
-                    }
-                    for (; ; ) {
-                        String update = readMessage(channel, buffer);
-                        System.err.println(update);
-                    }
-
-
-                } catch (IOException e) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 }
