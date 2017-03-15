7b8f6794ce588bd44f16dc62bdef23bc2f530f76
Mark Renouf <mark.renouf@gmail.com>
2017-03-15 10:21:16 -0400

    Updated demo to run device tracker

--- src/main/java/com/bitgrind/android/adb/Demo.java
+++ src/main/java/com/bitgrind/android/adb/Demo.java
@@ -8,11 +8,18 @@ import java.net.InetSocketAddress;
  * Created by mrenouf on 2/27/17.
  */
 public class Demo {
-    public static void main(String[] args) throws IOException, CommandException {
-        InetSocketAddress addr = new InetSocketAddress("123.0.0.1", 5037);
-        AsyncProtocol proto = new AsyncProtocol(new NetworkChannelSupplier(addr));
-        System.err.println("getVersion(): " + proto.getVersion());
-        System.err.println("listDevices(): " + proto.listDevices());
+    public static void main(String[] args) throws IOException, CommandException, InterruptedException {
+        InetSocketAddress addr = new InetSocketAddress("127.0.0.1", 5037);
+        NetworkChannelSupplier local = new NetworkChannelSupplier(addr);
+        AsyncProtocol proto = new AsyncProtocol(local);
+        //System.err.println("getVersion(): " + proto.getVersion());
+        //System.err.println("listDevices(): " + proto.listDevices());
         System.err.println("kill(): " + proto.kill());
+
+        DeviceTracker deviceTracker = new DeviceTracker(local, new AdbStarter("/home/mrenouf/Android/Sdk/platform-tools/adb"));
+        deviceTracker.addListener((action, serial, state) ->
+                System.out.format("%s %s %s\n", action, serial, state));
+        deviceTracker.start();
+        deviceTracker.join();
     }
 }
