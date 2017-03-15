4f3a7b7d36e22f1306f7ad6f10f3241b4d56a965
Mark Renouf <mrenouf@google.com>
2017-03-15 16:46:22 -0400

    Add timestamp and improve formatting

--- src/main/java/com/bitgrind/android/adb/Demo.java
+++ src/main/java/com/bitgrind/android/adb/Demo.java
@@ -1,8 +1,10 @@
 package com.bitgrind.android.adb;
 
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 /**
  * Created by mrenouf on 2/27/17.
@@ -14,11 +16,14 @@ public class Demo {
         AsyncProtocol proto = new AsyncProtocol(local);
         //System.err.println("getVersion(): " + proto.getVersion());
         //System.err.println("listDevices(): " + proto.listDevices());
-        System.err.println("kill(): " + proto.kill());
+        //System.err.println("kill(): " + proto.kill());
 
-        DeviceTracker deviceTracker = new DeviceTracker(local, new AdbStarter("/home/mrenouf/Android/Sdk/platform-tools/adb"));
+        // "/home/mrenouf/Android/Sdk/platform-tools/adb"
+        DeviceTracker deviceTracker = new DeviceTracker(local, new AdbStarter());
+        DateTimeFormatter fmt = DateTimeFormatter
+            .ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.MEDIUM);
         deviceTracker.addListener((action, serial, state) ->
-                System.out.format("%s %s %s\n", action, serial, state));
+                System.out.format("%s %8s %20s %s\n", fmt.format(LocalDateTime.now()), action, serial, state));
         deviceTracker.start();
         deviceTracker.join();
     }
